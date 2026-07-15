f132fce4c34ca9afc6f61071164f84b8c79ea42c
AishatOrewa <aishatwokili@gmail.com>
2026-07-15 22:54:15 +0000

    Created the button and function for power

--- PowerButtons.py
+++ PowerButtons.py
@@ -1,6 +1,55 @@
+#This is the tutorial on how to use the os library.
+# import os
+# shutdown = input("Do you want to shutdown? ").lower()
+# if shutdown == 'no':
+#   exit()
+# else:
+#   os.system("shutdown /s /t 1")
+
+#Recreating a window that has three button, each button has a function for Restart, Shutdown and logout.
+import tkinter
 import os
-shutdown = input("Do you want to shutdown? ").lower()
-if shutdown == 'no':
-  exit()
-else:
-  os.system("shutdown /s /t 1")
+from tkinter import messagebox
+
+window = tkinter.Tk()
+window.title('Power')
+
+window.configure(bg='#000000')
+frame = tkinter.Frame()
+
+def shutdown():
+  answer = messagebox.askyesno(
+    "Confirm Shutdown",
+    "Are you sure you want to shut down your computer?"
+  )
+  if answer:
+    os.system("shutdown /s /t 1")
+def restart():
+  reply = messagebox.askyesno(
+    "Confirm System Restart",
+    "Do you really want to Restart your system?"
+  )
+  if reply:
+    os.system("shutdown /r /t 1")
+def logout():
+  user = messagebox.askyesno(
+    "Confirm User LogOut",
+    "Do you really want to LogOut from your System?"
+  )
+  if user:
+    os.system("shutdown /l")
+PageTitle = tkinter.Label(frame, text='Power Options')
+PageTitle.pack()
+
+Shutdown = tkinter.Button(frame, text="Shutdown", command=shutdown)
+Shutdown.pack()
+
+Logout = tkinter.Button(frame, text='LogOut', command=logout)
+Logout.pack()
+
+Restart = tkinter.Button(frame, text='Restart', command=restart)
+Restart.pack()
+
+
+frame.pack()
+window.mainloop()
