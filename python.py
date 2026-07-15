ce78c3c14a46b6b7bc7eed1bd12c5033570ff52f
AishatOrewa <aishatwokili@gmail.com>
2026-07-15 11:13:47 +0000

    Relearining the basics

---
+++ python.py
@@ -0,0 +1,46 @@
+#string formatting
+# name = 'Aihat'
+# age = 18
+# dept = 'Computer Science'
+
+# print( f"My name is {name}, i'm {age} years old and i'm from the department of {dept}.")
+
+#slicing
+school = 'Polytechnic'
+# print(school[0:4])
+# print(school[4:8])
+# print(school[4:12])
+#print(school[:4])
+# print(school[2: :2])
+# print(school[::2])
+# print(school[5:9:2])
+# print(school[::-1])
+
+#write a python code to print out the first seven characters given a value University
+
+# school = 'University'
+# print(school[:7])
+# x = 'Polytechnic'
+# print(x[0:4] + x[4:8])
+# x = [1,2,3]
+# y = [3,4,5]
+# z = x[1] + y[2]
+# print(z)
+
+##Assignment
+#Write a python program to collect input from users that are integer and add them together and display their result
+#Write a python program that allows user to input a number for breadth and a number for height, if number for height is equal to number for breadth print square, otherwise print rectangle.
+
+#Assignment1
+# user1 = int(input("Input a random number: "))
+# user2 = int(input("Input a random number: "))
+# user_num = user1 + user2
+# print(f"Sum of user's number is {user_num}")
+
+#Assignment2
+# User3 = input("Input the value of the breadth: ")
+# User4 = input("Input the value of the length: ")
+# if User3 == User4 :
+#   print("The shape is a Square")
+# else:
+#   print("The shape is a Rectangle")
